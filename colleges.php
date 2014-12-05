8fe3d5ab7e156d439832cd3f624df3b0f73e3524
Jade America <jmarucut@gmail.com>
2014-12-04 21:34:29 -0800

    adding colleges.php

---
+++ colleges.php
@@ -0,0 +1,18 @@
+<?php include('inc/nav-inc.php');?>
+	<div class="span-p sm-pad-top sm-pad-btm">
+	<div class="container">
+	<div class="pink sixteen columns xlrg-pad-top xlrg-pad-btm">
+		<img src="assets/college-23.png" class="seven columns">
+		<div class="eight columns">
+		<h1 class="txt-wht middle med-pad-btm">Search for your school</h1>
+		<div class="clear"></div>
+		<form action="" class="middle">
+  			<input type="text" id="schoolSearch" />
+		</form>
+		</div>
+	</div>
+	</div>
+	</div>
+
+
+<?php include('inc/footer-inc.php');?>
